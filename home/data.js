3dbc492dd0722522c51bba7c768558b16f497348
chouchinghua <160987288+chouchinghua@users.noreply.github.com>
2024-04-19 11:52:13 +0800

    4/19true

--- home/data.js
+++ home/data.js
@@ -41,36 +41,13 @@ var APP_DATA = {
           "text": "Text"
         }
       ]
-    },
-    {
-      "id": "1-",
-      "name": "校門口",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        }
-      ],
-      "faceSize": 512,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
     }
   ],
   "name": "首頁全景圖",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
-    "viewControlButtons": false
+    "fullscreenButton": true,
+    "viewControlButtons": true
   }
 };
